833b74731fa2f0cb9a8207ea03d6d256f66f770b
Michael Hetzel <michael.e.hetzel@gmail.com>
2022-01-05 14:58:25 -0500

    Adding part1 and copied to part 2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AdventOfCode2021
 Repository to store all code related to the Advent of Code challenges for the year 2021.
+
+https://adventofcode.com/2021
---
+++ day-1/src/main/kotlin/Main.kt
@@ -0,0 +1,43 @@
+/*
+the sonar sweep report (your puzzle input) appears:
+each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
+
+Purpose: count the number of times a depth measurement increases from the previous measurement.
+*/
+import java.io.File
+
+fun part1(filePath: String): Int {
+    // Get to infer Int with Kotlin
+    var previousDepth = -1
+    var totalIncreases = 0
+
+    // Using file directly because this isn't real life
+    File(filePath).useLines { lines -> lines.forEach {
+        if (previousDepth != -1 && previousDepth < it.toInt())
+            totalIncreases += 1
+        previousDepth = it.toInt()
+    } }
+    return totalIncreases
+}
+
+fun part2(filePath: String): Int {
+    var previousDepth = -1
+    var totalIncreases = 0
+
+    // Using file directly because this isn't real life
+    File(filePath).useLines { lines -> lines.forEach {
+        if (previousDepth != -1 && previousDepth < it.toInt())
+            totalIncreases += 1
+        previousDepth = it.toInt()
+    } }
+    return totalIncreases
+}
+
+fun main(args: Array<String>) {
+    // Using val since this will be read-only
+    val path = System.getProperty("user.dir")
+    val part1Answer = part1("$path/src/main/kotlin/inputpart1.txt")
+    println("Part 1 Total Increases: $part1Answer")
+
+
+}
